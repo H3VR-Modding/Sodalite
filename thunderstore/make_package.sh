e1ba1886fd8d84fe3e9bd0d2ce94a67364522932
Nathan Gill <nrgill28@gmail.com>
2021-06-07 21:29:18 -0400

    use version tag as thunderstore manifest version

--- thunderstore/make_package.sh
+++ thunderstore/make_package.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 export TS_DIR="$(dirname "${BASH_SOURCE[0]}")"
+export VERSION=$(git describe --tags --abbrev=0 | sed -n 's/v\(.\+\)/\1/p')
 cd $TS_DIR
 
 # Delete the existing build if it exists
@@ -15,6 +16,9 @@ cp icon.png TEMP/icon.png
 cp ../README.md TEMP/README.md
 cp ../src/Sodalite/bin/Release/net35/Sodalite.dll TEMP/Sodalite/plugins/Sodalite.dll
 cp ../src/Sodalite/bin/Release/net35/Sodalite.Patcher.dll TEMP/Sodalite/patchers/Sodalite.Patcher.dll
+
+# Modify the version number
+sed -i "s/{VERSION}/$VERSION/g" TEMP/manifest.json
 
 # Zip the folder
 cd TEMP
