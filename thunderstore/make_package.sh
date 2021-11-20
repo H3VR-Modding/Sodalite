47b41996179f97d74b1044eceb8787c8d90776c2
Nathan Gill <nrgill28@gmail.com>
2021-11-20 00:35:30 -0500

    Update build workflow

--- thunderstore/make_package.sh
+++ thunderstore/make_package.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 export TS_DIR="$(dirname "${BASH_SOURCE[0]}")"
-export VERSION=$(git describe --tags --abbrev=0 | sed -n 's/v\(.\+\)/\1/p')
+export VERSION=$(git describe --tags --abbrev=0 | sed -n 's/\(.\+\)/\1/p')
 cd $TS_DIR
 
 # Delete the existing build if it exists
-rm nrgill28-Sodalite-*.zip
+rm nrgill28-Sodalite.zip
 
 # Create our temp folders
 mkdir -p TEMP/Sodalite/plugins
@@ -14,15 +14,15 @@ mkdir TEMP/Sodalite/patchers
 cp manifest.json TEMP/manifest.json
 cp icon.png TEMP/icon.png
 cp ../README.md TEMP/README.md
-cp ../src/Sodalite/bin/Release/net35/Sodalite.dll TEMP/Sodalite/plugins/Sodalite.dll
-cp ../src/Sodalite/bin/Release/net35/Sodalite.Patcher.dll TEMP/Sodalite/patchers/Sodalite.Patcher.dll
+cp ../src/Sodalite/bin/Runtime/net35/Sodalite.dll TEMP/Sodalite/plugins/Sodalite.dll
+cp ../src/Sodalite/bin/Runtime/net35/Sodalite.Patcher.dll TEMP/Sodalite/patchers/Sodalite.Patcher.dll
 
 # Modify the version number
 sed -i "s/{VERSION}/$VERSION/g" TEMP/manifest.json
 
 # Zip the folder
 cd TEMP
-zip -9r "../nrgill28-Sodalite-$VERSION.zip" *
+zip -9r "../nrgill28-Sodalite.zip" *
 
 # Delete the temp dir
 cd ..
