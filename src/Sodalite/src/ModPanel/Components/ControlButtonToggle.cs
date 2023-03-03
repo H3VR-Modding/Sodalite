07954450d27b0de85470aef13844e3fbaeb8f7b7
Nathan Gill <nrgill28@gmail.com>
2023-03-03 02:25:34 -0500

    Add log page message level filter

---
+++ src/Sodalite/src/ModPanel/Components/ControlButtonToggle.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using FistVR;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Sodalite.ModPanel.Components;
+
+/// <summary>
+/// Similar to <see cref="SodaliteBetterPointableButton"/> except that it toggles state on click and passes that state to a handler
+/// </summary>
+public class SodaliteToggleButton : FVRPointable
+{
+	[SerializeField] private bool BeginEnabled = false;
+	[SerializeField] private GameObject EnableOnToggle = null!;
+	[SerializeField] private Image Graphic = null!;
+
+	private Button _button = null!;
+	private bool _state;
+	private Color _originalGraphicTint;
+
+	/// <summary>
+	/// Event for when this button's value changes
+	/// </summary>
+	public event Action<bool>? OnValueChanged;
+
+	private void Awake()
+	{
+		_button = GetComponent<Button>();
+		if (Graphic != null) _originalGraphicTint = Graphic.color;
+		_state = BeginEnabled;
+		UpdateState();
+	}
+
+	/// <summary>
+	/// Sets the toggles state of this button
+	/// </summary>
+	/// <param name="value">If the button is enabled</param>
+	public void SetState(bool value)
+	{
+		if (_state == value) return;
+		_state = value;
+		UpdateState();
+		OnValueChanged?.Invoke(_state);
+	}
+
+	/// <summary>
+	/// Toggles the state of this button
+	/// </summary>
+	public void ToggleState()
+	{
+		_state = !_state;
+		UpdateState();
+		OnValueChanged?.Invoke(_state);
+	}
+
+	private void UpdateState()
+	{
+		if (EnableOnToggle != null) EnableOnToggle.SetActive(BeginEnabled != _state);
+		if (Graphic != null)
+		{
+			Graphic.color = BeginEnabled == _state ? _originalGraphicTint : Color.gray;
+		}
+	}
+
+	/// <summary>
+	///     Called when a hand starts hovering over this pointable
+	/// </summary>
+	public override void BeginHoverDisplay()
+	{
+		// Proxy a pointer enter to the button
+		_button.OnPointerEnter(null);
+	}
+
+	/// <summary>
+	///     Called when a hand stops hovering over this pointable
+	/// </summary>
+	public override void EndHoverDisplay()
+	{
+		// Proxy a pointer exit to the button
+		_button.OnPointerExit(null);
+	}
+
+	/// <summary>
+	///     Called per-frame per-hand that is pointing at this pointable
+	/// </summary>
+	/// <param name="hand">The hand pointing at this</param>
+	public override void OnPoint(FVRViveHand hand)
+	{
+		base.OnPoint(hand);
+		if (hand.Input.TriggerUp)
+			SetState(!_state);
+	}
+}
--- src/Sodalite/src/ModPanel/Pages/LogPage.cs
+++ src/Sodalite/src/ModPanel/Pages/LogPage.cs
@@ -1,7 +1,8 @@
-#pragma warning disable CS1591
+﻿#pragma warning disable CS1591
 using System;
 using System.Collections.Generic;
 using System.Text;
+using BepInEx.Configuration;
 using BepInEx.Logging;
 using Sodalite.ModPanel.Components;
 using UnityEngine;
@@ -22,12 +23,44 @@ public sealed class UniversalModPanelLogPage : UniversalModPanelPage, ISodaliteS
 	};
 	[SerializeField] private Text Log = null!;
 	[SerializeField] private int MaxLines = 0;
+
+	[SerializeField] private SodaliteToggleButton DebugToggle = null!;
+	[SerializeField] private SodaliteToggleButton InfoToggle = null!;
+	[SerializeField] private SodaliteToggleButton WarningToggle = null!;
+	[SerializeField] private SodaliteToggleButton ErrorToggle = null!;
+
 	private int _offset;
 	private float _scrollValue;
+	private readonly List<LogEventArgs> _cache = new();
 
-	private void Awake()
+	private void Start()
 	{
+		// Setup the initial state of the toggle buttons
+		var curFilter = Sodalite.Config.LogPanelFilter.Value;
+		DebugToggle.SetState((curFilter & LogLevel.Debug) != 0);
+		WarningToggle.SetState((curFilter & LogLevel.Warning) != 0);
+		ErrorToggle.SetState((curFilter & LogLevel.Error) != 0);
+		InfoToggle.SetState((curFilter & (LogLevel.Info | LogLevel.Message)) != 0);
+
+		// Register a whole bunch of listeners for the buttons so we can toggle the log panel filter setting
+		DebugToggle.OnValueChanged += b => SetOrClearFlag(b, LogLevel.Debug);
+		WarningToggle.OnValueChanged += b => SetOrClearFlag(b, LogLevel.Warning);
+		ErrorToggle.OnValueChanged += b => SetOrClearFlag(b, LogLevel.Error);
+		InfoToggle.OnValueChanged += b => SetOrClearFlag(b, LogLevel.Info | LogLevel.Message);
+
+		// Then register a listener for that config so we can update the panel when needed
+		Sodalite.Config.LogPanelFilter.SettingChanged += (_, _) => RebuildFilterCache();
+
+		// Finally do a rebuild and redraw all text
+		RebuildFilterCache();
 		UpdateText(true);
+
+		// Function used above to set or clear the log level flags
+		static void SetOrClearFlag(bool b, LogLevel flags)
+		{
+			if (b) Sodalite.Config.LogPanelFilter.Value |= flags;
+			else Sodalite.Config.LogPanelFilter.Value &= ~flags;
+		}
 	}
 
 	public void Scroll(Vector2 x)
@@ -37,7 +70,11 @@ public sealed class UniversalModPanelLogPage : UniversalModPanelPage, ISodaliteS
 
 		// Wait until we're scrolling at least one line
 		if (Mathf.Abs(_scrollValue) < 1) return;
-		_offset = Mathf.Clamp(_offset + (int) _scrollValue, 0, Sodalite.LogEvents.Count - 1);
+
+		// Clamp the offset so we can't scroll past the first or last lines
+		_offset = Mathf.Clamp(_offset + (int) _scrollValue, 0, Mathf.Max(0, _cache.Count - (MaxLines - 1)));
+
+		// Update text then move our scroll value counter one towards zero
 		UpdateText(false);
 		_scrollValue += _scrollValue > 0 ? -1 : 1;
 	}
@@ -46,7 +83,34 @@ public sealed class UniversalModPanelLogPage : UniversalModPanelPage, ISodaliteS
 	{
 		// If the user is currently offset the scrolling, keep their position in the log
 		if (_offset != 0) _offset += Sodalite.LogEventLineCount[evt];
+
+		// If this message matches our current filter, add it to our cache immediately
+		if ((evt.Level & Sodalite.Config.LogPanelFilter.Value) != 0)
+		{
+			_cache.Add(evt);
+		}
+
 		UpdateText(false);
+	}
+
+	public void RebuildFilterCache()
+	{
+		// Clear our current offset and cache, we can't keep it when changing the messages
+		_offset = 0;
+		_cache.Clear();
+
+		// Loop over all the events, adding them to the cache if they match
+		foreach (var evt in Sodalite.LogEvents)
+		{
+			// If this message matches our current filter, add it to our cache immediately
+			if ((evt.Level & Sodalite.Config.LogPanelFilter.Value) != 0)
+			{
+				_cache.Add(evt);
+			}
+		}
+
+		// Force a redraw
+		UpdateText(true);
 	}
 
 	internal void UpdateText(bool evenIfDisabled)
@@ -60,14 +124,14 @@ public sealed class UniversalModPanelLogPage : UniversalModPanelPage, ISodaliteS
 		for (int i = 0; count < MaxLines; i++)
 		{
 			// Make sure we're not hitting the start of the list
-			int idx = Sodalite.LogEvents.Count - (1 + i);
+			int idx = _cache.Count - (1 + i);
 			if (idx < 0) break;
 
 			// Get the ith line from the bottom, append it to the string, and count its lines
-			var evt = Sodalite.LogEvents[idx];
+			var evt = _cache[idx];
 
 			// Check if we're skipping this one
-			int lineCount = Sodalite.LogEventLineCount![evt];
+			int lineCount = Sodalite.LogEventLineCount[evt];
 			if (lineCount <= toSkip)
 			{
 				toSkip -= lineCount;
@@ -92,7 +156,7 @@ public sealed class UniversalModPanelLogPage : UniversalModPanelPage, ISodaliteS
 		}
 
 		// Remove the trailing newline and set the text
-		sb.Remove(sb.Length - 1, 1);
+		if (sb.Length > 0) sb.Remove(sb.Length - 1, 1);
 		Log.text = sb.ToString();
 	}
 }
